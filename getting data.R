08d43e1696a97210c6417dc93acea9096556492a
Clairetls <l.s.tsui@rug.nl>
2024-12-10 15:49:55 +0100

    nowhere closer (cry)

--- getting data.R
+++ getting data.R
@@ -1,8 +1,20 @@
 pedigree<-read.csv('updated pedigree.csv', sep=';')
-lifespan<-read.csv('lifespan.csv')
+lifespan<-read.csv('lifespan_28_5_24.csv')
 lastseen<-read.csv('lastseen.csv',sep=';')
 lifespan<-lifespan[,-c(1)]
 "%!in%" <-Negate("%in%")
+
+library(RODBC)
+DRIVERINFO <- "Driver={Microsoft Access Driver (*.mdb, *.accdb)};"
+
+#change to path of your database 
+MDBPATH <- "C:/Users/P309444/Downloads/database day 191124/SeychellesWarbler1.11.1.accdb"
+PATH <- paste0(DRIVERINFO, "DBQ=", MDBPATH)
+
+swdb<-odbcDriverConnect(PATH)
+
+eh<-read.csv('updated pedigree.csv', sep=';')
+eh<-filter(eh, is.na(eh$GenMumConfidence))
 
 library(tidyverse)
 pedigree<-left_join(pedigree,lifespan, by="BirdID")
@@ -129,11 +141,131 @@ write.csv(annualReprosuccess, "ars_90day.csv")
 
 
 #get r from leslie matrix 
+# 
+# lambda<-lastseen%>%
+#   group_by(PeriodYear)%>%
+#   distinct(BirdID, .keep_all = T)%>%
+#   summarise(popsize=n())%>%
+#   mutate(lambda=popsize/lag(popsize))
+
+
+#survival 
+
+lastseen_new<-sqlQuery(swdb, 'SELECT sys_StatusByFieldPeriod.BirdID, sys_StatusByFieldPeriod.FieldPeriodID, sys_StatusByFieldPeriod.Status, tblBirdID.BirthDate, tblFieldPeriodIDs.Island, tblFieldPeriodIDs.PeriodYear, tblBirdID.BirthFieldPeriodID, tblFieldPeriodIDs.PeriodEnd
+FROM (sys_StatusByFieldPeriod INNER JOIN tblBirdID ON sys_StatusByFieldPeriod.BirdID = tblBirdID.BirdID) INNER JOIN tblFieldPeriodIDs ON sys_StatusByFieldPeriod.FieldPeriodID = tblFieldPeriodIDs.FieldPeriodID;
+', stringsAsFactors=F)
+
+lastseen_new<-filter(lastseen_new, lastseen_new$Island=="CN")
+
+#save data new file to folder 
+write.csv(lastseen_new, 'breedstatuses_20112024.csv')
+
+#survival again 
+lastseen_new$birthyear<-as.numeric(substr(lastseen_new$BirthDate, 1,4))
+lastseen_new$age<-lastseen_new$PeriodYear-lastseen_new$birthyear
+
+#assign survival column
+lastseen_new$survival<-1
+lastseen_new$survival[lastseen_new$Status=="NS"]<-0
+
+
+#retain 2023 latest
+survival23<-filter(lastseen_new, lastseen_new$PeriodYear<=2023)
+alivebirds<-lastseen_new[(lastseen_new$Status!="NS" & lastseen_new$FieldPeriodID==186),]
+
+#removed birds that are still alive from analysis
+survivalanalysis<-filter(survival23, survival23$BirdID %!in% alivebirds$BirdID)
+survivalanalysis$PeriodEndYear<-as.numeric(str_sub(survivalanalysis$PeriodEnd, 1,4))
 
 
 
+#create ns for birds that have no NS 
+for(i in unique(survivalanalysis$BirdID)){
+  onebird<-filter(survivalanalysis, survivalanalysis$BirdID==i)
+  if(all(onebird$survival!=0)){
+    lastyear<-max(onebird$PeriodEndYear)#find the last year vector
+    PeriodYear<-max(onebird$PeriodYear)
+    age<-max(onebird$age)
+    birthyear<-unique(onebird$birthyear)
+    survivalanalysis<-survivalanalysis%>%
+      add_row(BirdID=i, PeriodEndYear=lastyear+1, PeriodYear=PeriodYear+1, Status="NS", age=age+1, survival=0, birthyear=birthyear)
+    #and then add a new row into the dataframe and assign the survival as 0
+  }
+}
+ 
+
+sex<-read.csv('sys_SexEstimates.csv', sep=';')
+sex<-sex[,c(1,2)]
+survivalanalysis<-left_join(survivalanalysis,sex, by="BirdID")
+
+#remove translocated bidrs 
+translocated<-sqlFetch(swdb, "tblTranslocations", stringsAsFactor=F)
+
+survivalanalysis2<-filter(survivalanalysis, survivalanalysis$BirdID %!in% translocated$BirdID)
 
 
+survivalanalysis2$PeriodEnd<-as.Date(survivalanalysis2$PeriodEnd, '%Y-%m-%d')
+
+fps<-readxl::read_excel('fps.xlsx')
+fps$PeriodEnd<-as.Date(fps$PeriodEnd, "%Y-%m-%d")
+fps$PeriodStart<-as.Date(fps$PeriodStart, "%Y-%m-%d")
+fps$seasonlength<-fps$PeriodEnd-fps$PeriodStart
+fps<-fps%>%
+  group_by(PeriodYear)%>%
+  mutate(season=case_when(seasonlength == max(seasonlength)~"main",
+                          seasonlength != max(seasonlength)~"winter"))
+fps<-as.data.frame(fps)
+fps<-filter(fps, fps$season=='main')
+# majorseason<-majorseason[majorseason$FieldPeriodID!=29,]
+# majorseason<-rbind(majorseason, fps[fps$FieldPeriodID==28,])
+# majorseason$PeriodEnd<-as.Date(majorseason$PeriodEnd, '%Y-%m-%d')
+
+test<-survivalanalysis2
+# test$PeriodEnd<-test$PeriodEnd+1
+
+
+#trial no 2
+test2<-merge(survivalanalysis2, fps, by.x = c("FieldPeriodID", 'PeriodYear', 
+                                              "PeriodEnd"), all.x=T)
+
+
+test<-test%>%
+  dplyr::left_join(fps, by=c("PeriodYear"), suffix = c('','.new'))%>%
+  mutate(FieldPeriodID = ifelse(is.na(FieldPeriodID), FieldPeriodID.new, FieldPeriodID),) %>%
+  select(-c(FieldPeriodID.new,))
+
+
+#remove the double NS 
+
+
+#take major season 
+er<-test%>%
+  group_by(BirdID, PeriodYear)%>%
+  filter(FieldPeriodID==max(FieldPeriodID))
+
+mightwork<-er
+
+
+
+hist(survivalanalysis$age)
+hist(physio$age_year)
+
+survprop<-survivalanalysis%>%
+  group_by(age)%>%
+  summarize(prop=length(age)/nrow(survivalanalysis))
+
+sum(survprop$prop)
+physprop<-physio%>%
+  group_by(age_year)%>%
+  summarize(prop=length(age_year)/nrow(physio))
+
+#check whether catches are proportional across ages and size 
+#from survival, 
+
+hist(physio$BodyMass)
+
+#is r the same? 
+#no its nr births minus nr deaths 
 
 
 
@@ -155,7 +287,5 @@ write.csv(annualReprosuccess, "ars_90day.csv")
 
 
 #i dont think i can do the above because of formula 
-
-
 
 
