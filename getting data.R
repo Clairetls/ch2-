5011331b8374b86bc3468badba05b8a91e56b825
Clairetls <l.s.tsui@rug.nl>
2025-01-17 14:53:27 +0100

    done fx

--- getting data.R
+++ getting data.R
@@ -27,11 +27,18 @@ pedigree<-filter(pedigree, pedigree$GenDadConfidence>=80 & pedigree$GenMumConfid
 
 #subset pedigree to exclude individuals older than 90 days 
 test<-filter(pedigree, pedigree$newlifespan>90|is.na(pedigree$newlifespan))
+#filter for 90 
+
+test2<-filter(pedigree, pedigree$newlifespan>364|is.na(pedigree$newlifespan))
+#filter for a year survival as per otto and day
 
 #NAs<-filter(test, is.na(test$newlifespan))  check NAs 
 filteredped<-test
+filteredped2<-test2  #a year
 
 filteredped$chickyear<-as.numeric(str_sub(filteredped$BirthDate, 7))
+
+filteredped2$chickyear<-as.numeric(str_sub(filteredped2$BirthDate, 7)) #a year
 
 dams<-list()
 for(i in filteredped$BirdID){
@@ -40,6 +47,18 @@ for(i in filteredped$BirdID){
   dams[[i]]<-dam
 }
 
+dams<-do.call(rbind.data.frame, dams)
+
+#for a year
+dams2<-list()
+for(i in filteredped2$BirdID){
+  dam2<-data.frame()
+  dam2<-filter(filteredped2, filteredped2$GeneticMother==i)
+  dams2[[i]]<-dam2
+}
+dams2<-do.call(rbind.data.frame, dams2)
+
+#fathers 
 sires<-list()
 for(i in filteredped$BirdID){
   sire<-data.frame()
@@ -47,13 +66,25 @@ for(i in filteredped$BirdID){
   sires[[i]]<-sire
 }
 
-dams<-do.call(rbind.data.frame, dams)
+
 sires<-do.call(rbind.data.frame, sires)
-
-
 sires$chickyear<-str_sub(sires$BirthDate, 7,10)
 
 
+#one year ped
+sires2<-list()
+for(i in filteredped2$BirdID){
+  sire2<-data.frame()
+  sire2<-filter(filteredped2, filteredped2$GeneticFather==i)
+  sires2[[i]]<-sire2
+}
+
+sires2<-do.call(rbind.data.frame, sires2)
+sires2$chickyear<-str_sub(sires2$BirthDate, 7,10)
+
+###########
+
+#90 days 
 dars<-data.frame()
 dars<-dams%>%
   group_by(GeneticMother, chickyear)%>%
@@ -67,6 +98,23 @@ names(dars)[names(dars) == 'GeneticMother'] <- 'BirdID'
 dars<-dars[,c("NestID","BirdID","chickyear","ars")]
 
 
+
+#one year dams################
+dars2<-data.frame()
+dars2<-dams2%>%
+  group_by(GeneticMother, chickyear)%>%
+  mutate(ars=length(chickyear))  #originally this row is summarize to make new df
+
+
+
+dars2<-as.data.frame(dars2)
+names(dars2)[names(dars2) == 'BirdID'] <- 'ChickBirdID'
+names(dars2)[names(dars2) == 'GeneticMother'] <- 'BirdID'
+dars2<-dars2[,c("NestID","BirdID","chickyear","ars")]
+dars2<-unique(dars2)
+
+#####################
+#90 days sire
 sars<-data.frame()
 sars<-sires%>%
   group_by(GeneticFather, chickyear)%>%
@@ -78,6 +126,20 @@ sars<-as.data.frame(sars)
 names(sars)[names(sars) == 'BirdID'] <- 'ChickBirdID'
 names(sars)[names(sars) == 'GeneticFather'] <- 'BirdID'
 sars<-sars[,c("BirdID","chickyear","ars","NestID")]
+
+
+#one year sire --------------
+sars2<-data.frame()
+sars2<-sires2%>%
+  group_by(GeneticFather, chickyear)%>%
+  mutate(ars=length(chickyear))  #originally this row is summarize to make new df
+
+sars2<-as.data.frame(sars2)
+names(sars2)[names(sars2) == 'BirdID'] <- 'ChickBirdID'
+names(sars2)[names(sars2) == 'GeneticFather'] <- 'BirdID'
+sars2<-sars2[,c("BirdID","chickyear","ars","NestID")]
+sars2<-unique(sars2)
+
 
 
 #fill in the gaps where they werent reproducing
@@ -107,7 +169,33 @@ idd<-idd%>%
 idd<-idd[,-c(2)]
 
 
-#dad turn
+#one year --------------------------------
+d2<-subset(lastseen, lastseen$BirdID %in% dars2$BirdID) #gets genetic mom
+d2<-d2[,c("BirdID","PeriodYear")]
+d2$chickyear<-d2$PeriodYear
+d2<-unique(d2)
+
+idd2<-data.frame()
+#i<-444
+for(i in unique(d2$BirdID)){
+  bird2<-filter(d2, d2$BirdID==i)
+  birds2<-filter(dars2, dars2$BirdID==i)
+  no2<-subset(bird2, bird2$chickyear %!in% birds2$chickyear)
+  no2<-unique(no2)
+  idd2<-rbind(no2,idd2)
+}
+idd2$ars<-0
+
+# names(idd)[names(idd) == 'PeriodYear'] <- 'chickyear'
+# names(idd)[names(idd) == 'FieldPeriodID'] <- 'BirthFieldPeriodID'
+
+idd2<-idd2%>%
+  mutate(NestID=NA)
+idd2<-idd2[,-c(2)]
+
+
+
+#dad turn---------------------------------
 s<-subset(lastseen, lastseen$BirdID %in% sars$BirdID) #gets genetic dad
 s<-s[,c("BirdID","PeriodYear")]
 s$chickyear<-s$PeriodYear
@@ -131,7 +219,33 @@ ids<-ids%>%
   mutate(NestID=NA,)
 ids<-ids[,-c(2)]
 
+######### one year -------------------------
+s2<-subset(lastseen, lastseen$BirdID %in% sars2$BirdID) #gets genetic dad
+s2<-s2[,c("BirdID","PeriodYear")]
+s2$chickyear<-s2$PeriodYear
+s2<-unique(s2)
 
+sars2<-as.data.frame(sars2)
+
+ids2<-data.frame()
+for(i in unique(s2$BirdID)){
+  bird2<-filter(s2, s2$BirdID %in% i)
+  birds2<-filter(sars2, sars2$BirdID %in% i)
+  no2<-subset(bird2, bird2$chickyear %!in% birds2$chickyear)
+  no2<-unique(no2)
+  ids2<-rbind(no2,ids2)
+}
+ids2$ars<-0
+# names(ids)[names(ids) == 'PeriodYear'] <- 'chickyear'
+# ids<-ids[,-c(2)]
+
+ids2<-ids2%>%
+  mutate(NestID=NA,)
+ids2<-ids2[,-c(2)]
+
+
+###########
+#rbind yes offspring and no offspring 
 Female_rs<-rbind(idd,dars)
 Malears<-rbind(ids,sars)
 
@@ -140,6 +254,18 @@ Malears$sex<-1
 annualReprosuccess<-rbind(Female_rs, Malears)
 
 write.csv(annualReprosuccess, "ars_90day.csv")
+
+# one year ----------------------------
+Female_rs2<-rbind(idd2,dars2)
+Malears2<-rbind(ids2,sars2)
+
+Female_rs2$sex<-0
+Malears2$sex<-1
+annualReprosuccess2<-rbind(Female_rs2, Malears2)
+
+write.csv(annualReprosuccess2, "ars_365.csv")
+
+
  
 #i can just add 0 for the individuals with no lifetime reproductive success later
 
@@ -197,6 +323,15 @@ survivalanalysis<-left_join(survivalanalysis,sex, by="BirdID")
 
 #remove translocated bidrs 
 translocated<-sqlFetch(swdb, "tblTranslocations", stringsAsFactor=F)
+# do you have any reason to assume the survival proportion in the translocated individuals differs from the main population 
+#what age did you translocate the birds 
+
+translocated<- translocated[,c("BirdID","ReleaseDate")]
+translocated$releaseyear<-as.numeric(str_sub(translocated$ReleaseDate, 1,4))
+translocated<-left_join(translocated, tblbirdid, by='BirdID')
+translocated$age<-translocated$releaseyear-translocated$birthyear
+hist(translocated$age)
+hist(translocated$birthyear,20)
 
 survivalanalysis2<-filter(survivalanalysis, survivalanalysis$BirdID %!in% translocated$BirdID)
 
@@ -269,7 +404,7 @@ write.csv(survivaldata, 'survival_updated.csv')
 
 
 latestlifespan<-read.csv('C:/PhD/Data/lifespan_171224.csv',stringsAsFactors = F)
-
+survivaldata<-read.csv('survival_updated.csv')
 test2<-left_join(survivaldata, latestlifespan, by='BirdID')
 test2<-test2[,-c(20)]
 check<-test2%>%
@@ -284,12 +419,14 @@ survivaldatafinal<-survivaldata2%>%filter(lifespan>90)
 
 #separate survival into cohorts 
 cohorts<- split(survivaldatafinal, survivaldatafinal$birthyear)
-
+# 
+# x<-as.data.frame(cohorts[[15]])
+# t<-0
 pxfunc<-function(x){
   x<-as.data.frame(x)
   onecohort<-data.frame()
   for(t in unique(x$age)){    # i should not need a counter for this
-    oneage<-filter(x, x$age==t)
+    oneage<-filter(x, x$age==t & survival==1)  #birds alive at age t #should work? 
     nextage<-filter(x, x$age==t+1)   #is including age t+1 enough? #there are only 39 birds ringed at age 2 so is negligible. 
     og<-length(oneage$BirdID)
     appeared<-length(setdiff(nextage$BirdID, oneage$BirdID)) #keep this 
@@ -298,43 +435,21 @@ pxfunc<-function(x){
     onerow<-data.frame(age=t,px=px)
     onecohort<-rbind(onecohort,onerow) #but need to remove last age 
   }
+  onecohort<-onecohort[-c(nrow(onecohort)),]
   return(onecohort)
 }
 
 survprob<-lapply(cohorts, pxfunc)
 
-#Remember 
-
-#dead<-length(setdiff(oneage$BirdID, nextage$BirdID))
-
-#what will happen in the last age??? 
-#next age will be empty, there will be none not dead, none appeared 
-
-#pseudocode 
-# for each cohort, find BirdIDs at age t
-#find same BirdIDs at age t+1 
+#for survival need to remove cohorts before 1992 and after 2020 due to lack of corresponding reproductive success data 
+#also need to remove last row for last age from all dataframes before making leslie matrix. 
 
 
+#translocations? 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#############################################################
+#age specific fertility 
 
 
 #get er 
@@ -347,10 +462,53 @@ tblbirdid$birthyear<-as.numeric(str_sub(tblbirdid$BirthDate, 1,4))
 tblbirdid<-tblbirdid[,c('BirdID', 'birthyear')]
 ars_90<-left_join(ars_90, tblbirdid, by='BirdID')
 ars_90$age<-ars_90$chickyear-ars_90$birthyear
+ars_90<-unique(ars_90)
 #there is still one individual with age -1 
 
 
-#test may work. should have gotten rid of individuals with double ns 
+
+################# one year --------
+ars_365<-read.csv('ars_365.csv')
+ars_365<-arrange(ars_365, ars_365$BirdID, ars_365$chickyear)
+ars_365<-ars_365[,-c(1)]
+
+ars_365<-left_join(ars_365, tblbirdid, by='BirdID')
+ars_365$age<-ars_365$chickyear-ars_365$birthyear
+ars_365<-unique(ars_365)
+
+
+
+#separate into cohorts 
+fertilitycohorts<-split(ars_90, ars_90$birthyear)
+
+fertcohort2<-split(ars_365, ars_365$birthyear)
+
+
+# x<-fertcohort2[['2011']]
+# t<-2
+
+#function to get age specific fertility 
+mxfunc<-function(x){
+  x<-as.data.frame(x)
+  onecohort<-data.frame()
+  for(t in unique(x$age)){    #for each age in a cohort,   
+    oneage<-filter(x, x$age==t & sex==0)  #filter: one dataset for each age of each cohort 
+    mx<-mean(oneage$ars)*0.5     #find the expected ars, 0.5 because female offspring of females only, but also fitness is halved
+    #unknowns: mx should be half female female offspring? or half all offspring? or complete female offspring
+    onerow<-data.frame(age=t,mx=mx)
+    onecohort<-rbind(onecohort,onerow) #but need to remove last age 
+  }
+  return(onecohort)
+}
+
+
+fx<-lapply(fertilitycohorts, mxfunc)  #90 day ars 
+fx2<-lapply(fertcohort2, mxfunc)  # one year ars 
+
+
+###########################################
+
+
 
 
 hist(survivalanalysis$age)
